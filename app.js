792441c985cb641b7fa9801d1aadae06183826a9
Jenny <yenny501ss@gmail.com>
2025-03-15 05:35:23 -0500

    Crear función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -2,19 +2,38 @@
 //Aquí deberás desarrollar la lógica para resolver el problema.
 let amigo =[];
 
+//Funcion para agregar amigos
 function agregarAmigo(){
-   //Capturar el valor del campo de entrada
+   //1. Capturar el valor del campo de entrada
    let inputAmigo = document.getElementById("amigo");
    let nombreamigo = inputAmigo.value;
    
-   //Validar la entrada
+   //2. Validar la entrada
    if(!nombreamigo){
       alert ("Por favor, inserte un nombre");
       return;
    }
-   //Actualizar el array de amigos
+   //3. Actualizar el array de amigos
    amigo.push(nombreamigo);
-   //Limpiar el campo de entrada
+   //4. Limpiar el campo de entrada
    inputAmigo.value = "";
    inputAmigo.focus();
+
+   MostrarAmigos()
+}
+
+//Funcion para actualizar la lista de amigos
+function MostrarAmigos(){
+   //1. Obtener el elemento de la lista
+   let listaAmigos = document.getElementById("listaAmigos");
+   //2. Limpiar la lista existente
+   listaAmigos.innerHTML = "";
+
+   //Iterar sobre el arreglo
+   for (let i=0; i < amigo.length; i++){
+      let item = document.createElement("li");
+      //Agregar elementos a la lista
+      item.textContent = amigo[i];
+      listaAmigos.appendChild(item);
+   }
 }
